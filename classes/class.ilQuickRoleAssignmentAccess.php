d34557c84d58cfd6b6551f13156d1f83e56b41e9
Michael Herren <michherren@gmail.com>
2015-10-15 13:42:36 +0200

    cleanup

--- classes/class.ilQuickRoleAssignmentAccess.php
+++ classes/class.ilQuickRoleAssignmentAccess.php
@@ -26,58 +26,5 @@ class ilQuickRoleAssignmentAccess {
 
 		return $rbacreview->isAssignedToAtLeastOneGivenRole($ilUser->getId(), $required_role);
 	}
-
-	/*public function getDevisionIdsOfCurrentUserWithSkillManagementPermission() {
-		global $ilUser, $rbacreview;
-		$arr_dev_id = array();
-
-		$arr_global_role_id = $rbacreview->assignedGlobalRoles($ilUser->getId());
-		foreach($arr_global_role_id as $role_id) {
-			//Prüfen ob Role ID und direkt zuweisen zu $role_dev_id
-			if($role_dev_id = emDevision::getDevisionIdBySkillManagerRoleId($role_id)) {
-				$arr_dev_id[] = $role_dev_id;
-			}
-		}
-		return $arr_dev_id;
-	}
-
-	public function hasCurrentUserSkillManagementPermission() {
-
-        //Reporting Access to Employees?
-        if(count(self::getDevisionIdsOfCurrentUserWithSkillManagementPermission()) > 0) {
-            return true;
-        }
-
-		return false;
-	}
-
-	public function isCourseRearrangementInProgress() {
-		return (ilTrainingProgramConfig::get(ilTrainingProgramConfig::F_CHANGE_LOCK) == 1);
-	}
-
-	public function hasCurrentUserEditUserProgramPermission() {
-		global $rbacreview, $ilUser;
-
-		$ilEducationManagementAccess = new ilEducationManagementAccess();
-		//Vorgesetzte
-		if($ilEducationManagementAccess->isCurrentUserSuperior()) {
-			return true;
-		}
-		//Lokale Kursadmininstratoren
-		if($ilEducationManagementAccess->hasCurrentUserLocalCrsAdminPermission()) {
-			return true;
-		}
-		//Zertitikatsadministratoren
-		$ilCourseCertificateAccess = new ilCourseCertificateAccess();
-		if($ilCourseCertificateAccess->hasCurrentUserAdministratePermission()) {
-			return true;
-		}
-		//Supperadministratoren
-		$global_roles = $rbacreview->assignedGlobalRoles($ilUser->getId());
-		if(in_array(2,$global_roles)) {
-			return true;
-		}
-
-		return false;
-	}*/
+	
 }
