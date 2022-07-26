c75cf04b662c255dfe8fe52354125bd0e23fb101
Nicolas Schäfli <nick@fluxlabs.ch>
2022-07-26 14:00:37 +0200

    Update contact info

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'xqra';
 $version = '1.1.0';
 $ilias_min_version = '6.0';
 $ilias_max_version = '7.999';
-$responsible = 'studer + raimann ag';
-$responsible_mail = 'support@studer-raimann.ch';
+$responsible = 'fluxlabs AG';
+$responsible_mail = 'support@fluxlabs.ch';
