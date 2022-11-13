6a4ff48ef3212ff5eec28b393eb07c86fb4a49a9
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-11-13 17:01:13 +0100

    fix/vendor-ilias-7

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'srag/dic' => array(
+            'pretty_version' => 'v0.10.3',
+            'version' => '0.10.3.0',
+            'reference' => '7b4208ac5d0733e13d443099aac45dd89cf14b29',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../srag/dic',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'srag/librariesnamespacechanger' => array(
+            'pretty_version' => 'v0.3.6',
+            'version' => '0.3.6.0',
+            'reference' => 'cf5f3be4341270fbe46a977bbd1403d744fb797a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../srag/librariesnamespacechanger',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
